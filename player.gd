6a0a3937ce522f357d29272cc678c2073b67ef34
Paul Rodriguez <Paulyd0123@gmail.com>
2026-03-06 08:46:03 -0500

    Finished character movement animations

--- player.gd
+++ player.gd
@@ -5,5 +5,8 @@ func _physics_process(_delta):
 	velocity = direction * 600
 	move_and_slide()
 	
-	
+	if velocity.length() > 0.0:
+		%HappyBoo.play_walk_animation()
+	else:
+		%HappyBoo.play_idle_animation()
 	
